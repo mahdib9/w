dfb47b44bc3444100bfd7130201b1af4fece2503
Mahdi <mahdib9@users.noreply.github.com>
2016-11-02 07:38:26 +0000

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 ob_start();
-define('API_KEY','توکن شما');
-$admin = "ایدی ادمین";
+define('API_KEY','216995156:AAGehMXHJAQCJzpWvsZmVU80kt_lEAK23Mg');
+$admin = "148553436";
 function bot($method,$datas=[]){
     $url = "https://api.telegram.org/bot".API_KEY."/".$method;
     $ch = curl_init();
